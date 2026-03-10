466cf9880bc4a31605adf431149a3b34551345d9
Grinchuk Nadezda <grennad991@gmail.com>
2026-03-10 18:40:11 +0100

    TaschenrechnerV3

--- src/TaschenrechnerV3.java
+++ src/TaschenrechnerV3.java
@@ -3,50 +3,46 @@ import java.util.Scanner;
 public class TaschenrechnerV3 {
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char operator;
-        int a;
-        int b;
+        boolean isCounting = true;
 
-        do {
+        while (isCounting) {
             System.out.println("Gib den Operator (+, -, *, /, %) ein: ");
-            operator = scanner.next().charAt(0);
+            char operator = scanner.next().charAt(0);
+
+            if (operator == '+' ||
+                operator == '-' ||
+                operator == '/' ||
+                operator == '*' ||
+                operator == '%'
+            ) {
+                System.out.println("Gib die erste Zahl ein: ");
+                int a = scanner.nextInt();
+                System.out.println("Gib die zweite Zahl ein: ");
+                int b = scanner.nextInt();
+
+                switch (operator) {
+                    case '+':
+                        System.out.println(a + " + " + b + " = " + (a + b));
+                        break;
+                    case '-':
+                        System.out.println(a + " - " + b + " = " + (a - b));
+                        break;
+                    case '*':
+                        System.out.println(a + " * " + b + " = " + (a * b));
+                        break;
+                    case '/':
+                        System.out.println(a + " / " + b + " = " + (double) a / b);
+                        break;
+                    case '%':
+                        System.out.println(a + " % " + b + " = " + (a % b));
+                        break;
+                    default:
+                        System.out.println("Gib den Operator (+, -, *, /, %) ein: ");
+                }
+            }
 
 
-            System.out.println("Gib die erste Zahl ein: ");
-            a = scanner.nextInt();
-
-            System.out.println("Gib die zweite Zahl ein: ");
-            b = scanner.nextInt();
-
-            if (operator == 'X') break;
-
-
-        } while (operator == '+' &&
-                 operator == '-' &&
-                 operator == '/' &&
-                 operator == '*' &&
-                 operator == '%');
-
-
-
-        switch (operator) {
-            case '+':
-                System.out.println(a + " + " + b + " = " + (a + b));
-                break;
-            case '-':
-                System.out.println(a + " - " + b + " = " + (a - b));
-                break;
-            case '*':
-                System.out.println(a + " * " + b + " = " + (a * b));
-                break;
-            case '/':
-                System.out.println(a + " / " + b + " = " + (double) a / b);
-                break;
-            case '%':
-                System.out.println(a + " % " + b + " = " + (a % b));
-                break;
-            default:
-                System.out.println("Falscher Operator.");
+            if (operator == 'X') isCounting = false;
         }
     }
 }
