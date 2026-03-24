ad79c8ee38d0ac349b0f4df6528b6d3095ba385b
Grinchuk Nadezda <grennad991@gmail.com>
2026-03-24 13:38:30 +0100

    Finish week 3 final assignment

--- src/KinoVerwaltungV2.java
+++ src/KinoVerwaltungV2.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
-import java.util.Objects;
 
 public class KinoVerwaltungV2 {
     static void main() {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         double budget;
         int activity;
         boolean leaveCinema = false;
+        ArrayList<String> chosenMovies = new ArrayList<>();
 
         do {
             System.out.println("Wie viel Geld hast du?");
@@ -16,7 +18,7 @@ public class KinoVerwaltungV2 {
 
         do {
             do {
-                System.out.printf("Was willst du als nächstes tun? (Du hast noch %.2f€)", budget);
+                System.out.printf("Was willst du als nächstes tun? (Du hast noch %.2f€)\n", budget);
                 System.out.println("1. Ticket kaufen");
                 System.out.println("2. Film ansehen");
                 System.out.println("3. Gewinnspiel");
@@ -25,43 +27,49 @@ public class KinoVerwaltungV2 {
 
             } while (activity <= 0 || activity > 4);
 
-            if(activity == 1) {
-                boolean breakProgram = false;
-                double ticketPrice = 15;
+            if (activity == 1) {
+                boolean stopActivity = false;
                 int choice;
-                ArrayList<Integer> chosenMovies = new ArrayList<>();
+                double ticketPrice = 0.00;
                 String[][] movieData = {
-                        {"1.", "Batman", "20:15", "1", "1"},
-                        {"2.", "Matrix", "22:00", "3", "3"},
-                        {"3.", "Matrix 2", "17:00", "2", "0"},
+                        {"1.", "Batman", "20:15", "1", "14.00", "1"},
+                        {"2.", "Matrix", "22:00", "3", "12.50", "3"},
+                        {"3.", "Matrix 2", "17:00", "2", "9.99", "0"},
+                        {"3.", "Matrix 2", "17:00", "2", "9.99", "0"},
                 };
-                String[] headers = {"Filmnr", "Filmname", "Uhrzeit", "Saal", "Restplätze"};
-
+                String[] headers = {"Filmnr", "Filmname", "Uhrzeit", "Saal", "Preis", "Restplätze"};
+                String[] formats = {"%-10s ", "%-20s ", "%-10s ", "%-6s ", " %-10s ", "%s"};
 
                 do {
-                    System.out.printf("%-10s %-20s %-10s %-6s %s %n", headers);
-                    System.out.println("------------------------------------------------------------");
-                    for (String[] movieDatum : movieData) {
-                        System.out.printf("%-10s %-20s %-10s %-6s %s \n",
-                                movieDatum[0],
-                                movieDatum[1],
-                                movieDatum[2],
-                                movieDatum[3],
-                                Integer.parseInt(movieDatum[4]) == 0 ? "ausgebucht" : "verfügbar");
+                    for (int i = 0; i < formats.length; i++) {
+                        System.out.printf(formats[i], headers[i]);
                     }
-                    System.out.println("------------------------------------------------------------");
+                    System.out.println();
+                    System.out.println("-------------------------------------------------------------------------");
+                    for (int i = 0; i < movieData.length; i++) {
+                        for (int j = 0; j < movieData[i].length - 1; j++) {
+                            System.out.printf(formats[j], movieData[i][j]);
+                        }
+                        String status = Integer.parseInt(movieData[i][5]) == 0 ?
+                                "ausgebucht" :
+                                "verfügbar";
+                        System.out.printf("%s", status);
+                        System.out.println();
+                    }
+                    System.out.println("-------------------------------------------------------------------------");
                     do {
                         System.out.println("Welchen (nicht ausgebuchten) Film möchtest du sehen? (0 zum abbrechen)");
                         choice = scanner.nextInt();
-                        if (choice == 0) breakProgram = true;
+                        if (choice == 0) stopActivity = true;
 
                     } while (choice != 0 && ((choice < 1 ||
                             choice > movieData.length) ||
                             budget < ticketPrice ||
-                            movieData[choice - 1][4].equals("0")));
+                            movieData[choice - 1][5].equals("0")));
 
                     if (choice != 0) {
-                        int availableTickets = Integer.parseInt(movieData[choice - 1][4]);
+                        ticketPrice = Double.parseDouble(movieData[choice - 1][4]);
+                        int availableTickets = Integer.parseInt(movieData[choice - 1][5]);
                         int amountTickets;
                         boolean isEnoughMoney;
                         String ticketText = (availableTickets > 1) ? "Tickets" : "Ticket";
@@ -71,43 +79,85 @@ public class KinoVerwaltungV2 {
                                     availableTickets, ticketText, ticketPrice);
                             amountTickets = scanner.nextInt();
 
-                            isEnoughMoney = budget - (amountTickets * ticketPrice) >= 0 && amountTickets <= availableTickets;
+                            isEnoughMoney = budget - (amountTickets * ticketPrice) >= 0 && amountTickets <= availableTickets && amountTickets > 0;
                             if (isEnoughMoney) {
                                 for (int i = 0; i < amountTickets; i++) {
-                                    chosenMovies.add(choice - 1);
+                                    chosenMovies.add(movieData[(choice - 1)][1]);
                                 }
                                 double ticketsPrice = amountTickets * ticketPrice;
                                 budget -= ticketsPrice;
                                 System.out.printf("Du kaufst %d Tickets um %.2f€ und hast jetzt noch %.2f€ \n",
                                         amountTickets, ticketsPrice, budget);
-                                movieData[choice - 1][4] = String.valueOf(Integer.parseInt(movieData[choice - 1][4]) - amountTickets);
+                                movieData[choice - 1][5] = String.valueOf(Integer.parseInt(movieData[choice - 1][5]) - amountTickets);
                             }
                         } while (amountTickets > availableTickets || amountTickets <= 0 || !isEnoughMoney);
                     }
-                } while (!breakProgram);
+                } while (!stopActivity);
             }
-            if(activity == 2) {
+            if (activity == 2) {
+                boolean stopActivity = false;
+                int choice;
+                do {
+                    do {
+                        System.out.println("Welchen Film (für den du noch ein Ticket hast) möchtest du sehen? (0 für abbrechen)");
+                        if (!chosenMovies.isEmpty()) {
+                            for (int i = 0; i < chosenMovies.size(); i++) {
+                                System.out.printf("%d. %s\n", i + 1, chosenMovies.get(i));
+                            }
+                        }
+                        choice = scanner.nextInt();
 
+                    } while (choice < 0 || choice > chosenMovies.size());
+                    if (choice == 0) {
+                        stopActivity = true;
+                    } else {
+                        System.out.printf("Du schaust dir den Film %s an. Viel Spaß!\n", chosenMovies.get(choice - 1));
+                        chosenMovies.remove(choice - 1);
+                    }
+                } while (!stopActivity);
             }
-            if(activity == 3) {
+            if (activity == 3) {
+                int randomNumber;
+                int summ = 0;
+                boolean stopActivity = false;
+                double ticketPrice = 5;
+                double winPrice = 25;
+                int choice;
 
+                do {
+                    System.out.printf("Ein Versuch kostet %.0f€, und der Gewinn beträgt %.0f€.\n", ticketPrice, winPrice);
+                    System.out.printf("Du hast %.0f€.\n", budget);
+                    if (budget < 5) {
+                        System.out.println("Nicht genug Geld.");
+                        break;
+                    }
+                    do {
+                        System.out.print("Drücke 1, um zu spielen, und 0, um zu abbrechen).\n");
+                        choice = scanner.nextInt();
+                    } while ((choice < 0 || choice > 1) && budget > 5);
+
+                    if (choice == 0) {
+                        stopActivity = true;
+                    } else {
+                        randomNumber = random.nextInt(10, 1001);
+                        System.out.println("Du machst beim Gewinnspiel mit!");
+                        System.out.printf("Deine Glückszahl ist: %d \n", randomNumber);
+                        budget -= 5;
+                        boolean isNumber = true;
+                        while (isNumber) {
+                            summ += randomNumber % 10;
+                            randomNumber = randomNumber / 10;
+                            isNumber = randomNumber != 0;
+                        }
+                        boolean isWin = summ > 16;
+                        String biggerOrSmaller = isWin ? "grosser" : "kleiner";
+                        System.out.printf("Die Ziffernsumme deiner Glückszahl ist %d und %s als 16.\n", summ, biggerOrSmaller);
+                        System.out.printf("%s", isWin ? "Du hast gewonnen!\n" : "Leider nicht gewonnen.\n");
+                        summ = 0;
+                        if (isWin) budget += 20;
+                    }
+                } while (!stopActivity);
             }
-            if(activity == 4) {
-
-            }
-        } while (leaveCinema);
-
-
-
-
-
-        /*
-         *
-         * Ich muss alle Preise auf double ändern.
-         * Es müssen keine negativen Zahlen sein
-         * Das Programm soll nicht weiterlaufen, wenn nicht genug Tickets vorhanden sind.
-         * Ich muss darauf achten, dass genug Geld vorhanden ist.
-         *
-         */
+        } while (!(activity == 4));
     }
 }
